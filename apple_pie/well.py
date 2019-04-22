e2543fff32197f6c7b403da4cb82e54843530cc6
Amelia <brown.amelia@gmail.com>
2019-04-22 10:11:18 -0400

    go to change condit to use pd.DataFrame

--- apple_pie/well.py
+++ apple_pie/well.py
@@ -1,8 +1,5 @@
-
-# from .ap_utils import *
 from brutils import *
 import pandas as pd
-import dfgui
 
 from . import hdings
 class Well :
@@ -13,15 +10,12 @@ class Well :
 
         #self.raw_data = Well.lever_csv_to_dict(csv_path)
         self.cdf = pd.read_csv(csv_path)
-        # print(type)
+
         self.cdf[hdings.WELL] = self.name
         self.cdf[hdings.DIST] = np.nan
 
 
-        ## fix distance columns
 
-        # self.cell_count = len(self.raw_data.keys())/self.exper.COL_COUNT
-        # self.cell_count = len(self.raw_data)
         CELL_IDFYIER = 'Track ID'
         self.cell_count = set(self.cdf[CELL_IDFYIER])
 
@@ -39,31 +33,12 @@ class Well :
 
 
     def create_dists(self) :
-        # print('create_dists')
-        # count = 0
+
         self.cdf = self.cdf.groupby(hdings.T_ID).apply(create_df_dists2,hdings.X,hdings.Y,hdings.DIST,hdings.FRAME)
-        # for i,g in self.cdf.groupby(hdings.T_ID) :
-        #     # self.g = g
-        #     print('create_df_dists: cell {}'.format(g[hdings.T_ID].iloc[0]))
-        #
-        #     create_df_dists2(g,hdings.X,hdings.Y,hdings.DIST,hdings.FRAME)
-        #     count +=1
-        #print('cell count: {}'.format(count))
-        #temp_df = pd.DataFrame(self.cdf.to_records(index=False)) # multiindex become columns and new index is integers only
 
-        # dfgui.show(temp_df)
-
-        # print(self.cdf.columns)
-
-        # print(self.cdf)
-
-        # print(self.cdf[hdings.DIST])#, hdings.FRAME])
-        # self.np_df = self.cdf.values
         self.cdf.reset_index(drop=True)
-        # self.one = self.cdf.iloc[:900, :]
-        # self.two = self.cdf.iloc[900:, :]
 
-    # TODO deal with old/new function in a reasonable way
+
 
     @staticmethod
     def lever_csv_to_dict_old(csv_path):
@@ -81,22 +56,5 @@ class Well :
                 col_dict[col_tuple_key] = cols[ce + co][4:]
 
         return col_dict
-
-    @staticmethod
-    def lever_csv_to_dict(csv_path):
-        """
-            output with all cells in same columns, labelled with track id
-            returns cells -> {cell_id : pd.df}
-        """
-        CELL_IDFYIER = 'Track ID'
-
-        df = pd.read_csv(csv_path)
-
-        cell_ids = set(df[CELL_IDFYIER])
-        cells = {}
-        for cell_id in cell_ids:
-            cells[cell_id] = df[df[CELL_IDFYIER] == cell_id]
-
-        return cells
 
 
--- new_format3.py
+++ new_format3.py
@@ -1,13 +1,10 @@
 import pandas as pd
-import brutils as br
-import numpy as np
-import apple_pie as ap
-import brutils as br
+
+
 
 import sys
 import os
-# sys.path.insert(0, "../")
-import apple_pie as ap
+
 
 import apple_pie as ap
 import brutils as br
@@ -15,8 +12,8 @@ import brutils as br
 
 from matplotlib import pyplot as plt
 import numpy as np
-from IPython.display import display as idisp
-# %matplotlib notebook
+
+
 from IPython import get_ipython
 ipy = get_ipython()
 ipy.magic('matplotlib osx')
@@ -49,51 +46,29 @@ class Butts(object):
         self.grouped = grouped
         self.groups = list(grouped.groups.keys())
         self.ind = 0
+
+
+
         plt.figure(figsize=(10, 5))
-
         self.ax = plt.gca()
-
         self.plot()
-        # plt.plot(self.cur_group()[x], self.cur_group()[y], 'bo')
-        # self.ax.set_xlim(0, 120)
-        # self.ax.set_ylim(-10, 100)
-        # plt.show()
 
-        #         self.l, = self.cur_group().plot(x=ap.hdings.FRAME,y=ap.hdings.DIST, kind="scatter")
-        # self.l, = plt.plot(self.cur_group()[ap.hdings.FRAME],self.cur_group()[ap.hdings.DIST], 'bo')
-        # self.ax = plt.gca()
-        # plt.plot(self.cur_group()[ap.hdings.FRAME], self.cur_group()[ap.hdings.DIST], 'bo')
 
-    #@staticmethod
+    def plot(self) :
+        self.ax.cla()
+        self.ax.set_title('Cell {}'.format(self.groups[self.ind]))
+        self.cur_group().plot(x, y, kind='scatter', ax=self.ax)
+        self.ax.set_xlim(0, 120)
+        self.ax.set_ylim(-10, 100)
+        plt.draw()
+
     def on_key_press(self,event):
-        # print('butts')
-        # print(event.key)
-        # print(self)
         if event.key == "left" :
-            # print('l')
             self.decr()
-
-            # # for i, line in enumerate(self.ax.lines):
-            # # self.ax.lines.pop(i)
-            # # line.remove()
-            # # self.ax.lines.pop(0).remove()
-            # self.ax.cla()
-            # self.ax.plot(self.cur_group()[x], self.cur_group()[y], 'bo')
-            # self.ax.set_xlim(0, 120)
-            # self.ax.set_ylim(-10, 100)
-            # plt.draw()
             self.plot()
-
         elif event.key == "right" :
-            # print('r')
             self.incr()
             self.plot()
-            # self.ax.cla()
-            # self.ax.plot(self.cur_group()[x], self.cur_group()[y], 'bo')
-            # self.ax.set_xlim(0, 120)
-            # self.ax.set_ylim(-10, 100)
-            # plt.draw()
-
 
     def __str__(self) :
         return(str(self.groups))
@@ -111,87 +86,22 @@ class Butts(object):
 
 
     def next(self, event):
-        #         print('next')
         self.incr()
         self.plot()
 
-        #for i, line in enumerate(self.ax.lines):
-            #self.ax.lines.pop(i)
-            #line.remove()
-        #self.ax.lines.pop(0).remove()
-        # self.ax.cla()
-        # #self.ax.plot(self.cur_group()[x], self.cur_group()[y], 'bo')
-        # self.cur_group().plot(x,y,kind='scatter', ax=self.ax)
-        # self.ax.set_xlim(0, 120)
-        # self.ax.set_ylim(-10, 100)
-
-        # l = lines[0]
-        # l.remove()
-        # del l
-        # del lines
-        # # not releasing memory
-        # ax.cla()  # this does release the memory, but also wipes out all other lines.
-
-        # self.plot()
-        # for i, line in enumerate(self.ax.lines):
-        #     self.ax.lines.pop(i)
-        #     line.remove()
-        # self.ax.plot(self.cur_group()[ap.hdings.FRAME], self.cur_group()[ap.hdings.DIST], 'bo')
-
-    #         self.plot2()
-    def plot(self) :
-        self.ax.cla()
-        # self.ax.plot(self.cur_group()[x], self.cur_group()[y], 'bo')
-        # plt.title('Cell {}'.format(self.groups[self.ind]),loc='center')
-        # plt.title('Cell {}'.format(self.groups[self.ind]),loc='center')
-        # plt.title('Cell {}'.format(self.groups[self.ind]),loc='center')
-        self.ax.set_title('Cell {}'.format(self.groups[self.ind]))
-        self.cur_group().plot(x, y, kind='scatter', ax=self.ax)
-        self.ax.set_xlim(0, 120)
-        self.ax.set_ylim(-10, 100)
-
-        plt.draw()
-
-
     def prev(self, event):
-        #         print('prev')
         self.decr()
         self.plot()
-        # self.ax.cla()
-        # self.ax.plot(self.cur_group()[x], self.cur_group()[y], 'bo')
-        # self.ax.set_xlim(0, 120)
-        # self.ax.set_ylim(-10, 100)
-        # # self.plot()
-        # self.ax.plot(self.cur_group()[ap.hdings.FRAME], self.cur_group()[ap.hdings.DIST], 'bo')
-        #
-        # self.plot2()
 
-    # def plot(self):
-    #     self.cur_group().plot(x,y)
-    #     plt.show()
-        #         self.ax.set_ylim(0,10)
-        # self.ax.plot(self.cur_group()[ap.hdings.FRAME], self.cur_group()[ap.hdings.DIST], 'bo')
 
-    #         plt.draw()
-    # self.l.set_xdata(self.cur_group()[ap.hdings.FRAME])
-    # self.l.set_ydata(self.cur_group()[ap.hdings.DISTANCE])
-    # plt.draw()
-    # self.fig.canvas.draw()
-    # self.fig.canvas.flush_events()
 
     def cur_group(self):
-        # print(self.groups[self.ind])
         return self.grouped.get_group(self.groups[self.ind])
 
 
 
-# grouped = w.cdf.groupby(ap.hdings.T_ID)
 
 gb = Butts(grouped,x,y)
-
-
-# fig, ax = plt.subplots()
-# plt.subplots_adjust(bottom=0.2)
 
 axprev = plt.axes([0.7, 0.05, 0.1, 0.075])
 axnext = plt.axes([0.81, 0.05, 0.1, 0.075])
@@ -202,44 +112,4 @@ bprev.on_clicked(gb.prev)
 
 plt.gcf().canvas.mpl_connect("key_press_event", gb.on_key_press)
 
-# plt.figure()
-#
-#
-# def get_g(grouped,groups,i) :
-#     return grouped.get_group(groups[i])
-# br.loc_print()
-#
-#
-# inputt = None
-# output = None
-#
-# for i,g in w.cdf.groupby(ap.hdings.T_ID) :
-#     inputt = g
-#     # print(type(g))
-#     # ouptut =
-#     br.create_df_dists2(g.copy(),ap.hdings.X,ap.hdings.Y,ap.hdings.DIST,ap.hdings.FRAME)
-#     #print(g)
-#
-#     # output = sum([10,10])
-#
-#     print(output)
-#
-#
-#     # ln = 24
-#     # fn = 'new_format.py'
-#     # if output is None:
-#     #     print('(new_format.py) {}: output is None'.format(ln))
-#     # else:
-#     #     print('(new_format.py) {}: output is something'.format(ln))
-#     break
-#
-#
-# print(br.rotate([[1]]))
-#
-# print(br.create_df_dists2(w.cdf,ap.hdings.X,ap.hdings.Y,ap.hdings.DIST,ap.hdings.FRAME))
-# # w.cdf.groupby(ap.hdings.T_ID).apply(br.create_df_dists2,ap.hdings.X,ap.hdings.Y,ap.hdings.DIST,ap.hdings.FRAME)
-# print(output)
-
-
-# br.dtoc(cd_tic)
 
